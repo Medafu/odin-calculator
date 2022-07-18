26866932f573b005eb617fd52549090ae696fc60
Medafu <justrayzzin@gmail.com>
2022-07-18 19:15:04 +0300

    finished evaluating by pairs

--- script.js
+++ script.js
@@ -30,7 +30,40 @@ function operate(num1, num2, operator) {
 }
 
 let display = document.querySelector(".display")
+display.textContent = "0"
+
+let buttons = document.querySelectorAll(".num")
+buttons.forEach(button => {
+    button.addEventListener('click', e => {
+        if (display.textContent == "0") {
+            return display.textContent = e.target.textContent
+        }
+
+        return display.textContent += e.target.textContent
+    })
+})
+
 let clear = document.querySelector(".clear")
 clear.addEventListener('click', () => {
-    display.textContent = ""
+    display.textContent = "0"
+})
+
+let operators = document.querySelectorAll(".op")
+operators.forEach(e => {
+    e.addEventListener('click', e => {
+        let eval = display.textContent.split(" ")
+        if (eval.length == 3) {
+            let first = parseInt(eval[0])
+            let operator = eval[1]
+            let second = parseInt(eval[2])
+            let result = operate(first, second, operator)
+            return display.textContent = result + " " + e.target.textContent + " "
+        }
+        return display.textContent += " " + e.target.textContent + " " 
+    })
+})
+
+let equal = document.querySelector("#equal")
+equal.addEventListener('click', e => {
+    
 })
